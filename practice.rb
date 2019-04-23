c5ad2dde224e541b7058086f572b41caf67a1df2
rarrunategu1 <parker.rose@ymail.com>
2019-04-23 17:46:50 -0400

    went over a few data types

---
+++ practice.rb
@@ -0,0 +1,34 @@
+print "I love to practice"
+print " ruby."
+puts ""
+puts "Practice is key."
+
+puts ""
+puts "This is how we can draw a shape:"
+puts "--------------------------------"
+puts "   /|"
+puts "  / |"
+puts " /__|"
+
+puts ""
+puts "Practice with variables:"
+puts "-----------------------"
+character_name = "Pippy"
+vegetable_quantity = "35"
+puts (""+ character_name + " Piper picked")
+puts ("" + vegetable_quantity + " pickled peppers,")
+character_name = "Peter"
+puts ("but " + character_name + " Piper needed")
+puts ("more than " + vegetable_quantity + " pickled peppers")
+puts "for the salad."
+
+puts ""
+puts "Practice with data types:"
+puts "-----------------------"
+stringname = "Mike"
+positiveNum = 34
+negativeNum = -34
+floatgpa = 3.2
+booleanismale = true
+booleanistall = false
+nilflaws = nil
