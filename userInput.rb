f981f61546602021fb59529571ef9a476b26f99a
rarrunategu1 <parker.rose@ymail.com>
2019-04-25 20:41:38 -0400

    Getting user iput and storing input in a variable

---
+++ userInput.rb
@@ -0,0 +1,8 @@
+puts ""
+puts "Getting user input:"
+puts "-----------------------"
+puts "Enter your name: "
+name = gets.chomp()
+puts "Enter your age: "
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age)
