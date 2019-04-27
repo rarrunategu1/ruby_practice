ca95e4a3b2260b86e452269449dd2720ab469a96
rarrunategu1 <parker.rose@ymail.com>
2019-04-27 13:53:44 -0400

    madLibGames allows user to enter a verb, plural noun and an infinitive verb to change the sentence provided.

---
+++ madLibGame.rb
@@ -0,0 +1,16 @@
+puts "Let's play a madlibs game with the sentence below"
+puts ""
+puts "Roses are red"
+puts "Violets are blue"
+puts "I have some coding to do"
+puts ""
+puts "Please enter a color: "
+color = gets.chomp()
+puts "Please enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Please enter a verb"
+infinitiveVerb = gets.chomp()
+puts ""
+puts ("Rose are " + color)
+puts ("" + plural_noun + " are blue")
+puts ("I have some "+ infinitiveVerb +" to do")
