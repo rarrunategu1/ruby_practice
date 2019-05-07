f3ccb48bb4c53193ed467d130a51a307fdad8c4f
rarrunategu1 <parker.rose@ymail.com>
2019-05-07 11:26:15 -0400

    using a for loop in different ways

---
+++ forloop.rb
@@ -0,0 +1,17 @@
+fruits = ["banana", "cherry", "pineapple", "strawberry"]
+
+for fruit in fruits
+  puts fruit
+end
+
+fruits.each do |fruit|
+puts fruit
+end
+
+for index in 0..5
+  puts index
+end
+
+6.times do |index|
+  puts index
+end
