7d64e4f1194361edd0db1b36ab3bd8d7fddc27e4
rarrunategu1 <parker.rose@ymail.com>
2019-05-07 14:55:37 -0400

    readWrite from beginning of opened file

--- writingToFiles.rb
+++ writingToFiles.rb
@@ -6,3 +6,9 @@ end
 File.open("index.html", "w") do |file|
 file.write("<h1>Hello World!</h1>")
 end
+  #readwrite from beginning of file
+File.open("employees.txt", "r+") do |file|
+  #reads first line
+  file.readline()
+  file.write("Override")
+end
