a85c624e0531840e501fc54e3e83757d37b251ea
rarrunategu1 <parker.rose@ymail.com>
2019-05-08 19:17:30 -0400

    using initialize method to refactor code for reusability

--- classesAndObjects.rb
+++ classesAndObjects.rb
@@ -1,21 +1,25 @@
 class Book
   #attributes
   attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
 end
-
 #creating objects
 
 #create new book object
-book1 = Book.new()
-book1.title = "Harry Potter"
-book1.author = "JK Rowling"
-book1.pages = 400
+book1 = Book.new("Harry Potter", "JK Rowling", 400)
+#book1.title = "Harry Potter"
+#book1.author = "JK Rowling"
+#book1.pages = 400
 
 #create another new book object
-book2 = Book.new()
-book2.title = "Living the Science of Mind"
-book2.author = "Ernest Holmes"
-book2.pages = 434
+book2 = Book.new("Living the Science Of Mind", "Ernest Holmes", 434)
+#book2.title = "Living the Science of Mind"
+#book2.author = "Ernest Holmes"
+#book2.pages = 434
 
 puts book1.title
 puts book2.title
